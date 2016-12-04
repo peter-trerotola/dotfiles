a6d53d00ee3621587111cb9c3da851ecff2ec736
Peter Trerotola <peter.trerotola@gmail.com>
2016-12-03 19:22:55 -0800

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -p "Do you want to update first? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get update
+	sudo apt-get upgrade
+fi
+for install in system i3 vim tmux zsh ssh; do
+	/bin/bash $install/setup
+done
